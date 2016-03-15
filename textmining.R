6f3649dec5e47466db6e6d52cffb38758952694a
Marcel Bergmann <marcel.bergmann@capgemini.com>
2016-03-15 18:54:32 +0100

    More preprocessing + list of naughty words

--- textmining.R
+++ textmining.R
@@ -2,13 +2,28 @@ library(NLP)
 library(openNLP)
 library(RWeka)
 library(magrittr)
+library(tm) # For text mining
+library(SnowballC) # For Stemming
 
-cname <- file.path("C:/Users/mbergman/Documents/Privat/Coursera/Capstone/Project", "texts")
+############################
+###### Preprocessing #######
+############################
 
+cname <- file.path("C:/Users/mbergman/Documents/Privat/Coursera/Capstone/DataScienceCapstone", "texts")
 corp <- Corpus(DirSource(cname))
 
 corp <- tm_map(corp, removePunctuation)
-corp <- tm_map(corp, stripWhitespace)
 corp <- tm_map(corp, content_transformer(tolower))
+naughtywords <- scan("en", what = "String")
+corp <- tm_map(corp, removeWords, naughtywords)
+#corp <- tm_map(corp, stemDocument)
+corp <- tm_map(corp, removeNumbers)
+corp <- tm_map(corp, removeWords, stopwords("english"))
+corp <- tm_map(corp, stripWhitespace)
 
-# corp <- tm_map(corp, removeWords, stopwords("english"))
+
+
+############################
+####### Exploration ########
+############################
+dtm <- DocumentTermMatrix(corp)
